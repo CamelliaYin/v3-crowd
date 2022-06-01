cb0f0fb62c1ae71b9eb4580bcfa5a1ae3e4b7566
CamelliaYin <ry00042@surrey.ac.uk>
2022-06-01 13:15:58 +0100

    pretrain ran

--- src/train.py
+++ src/train.py
@@ -631,9 +631,9 @@ def run(**kwargs):
 
 if __name__ == "__main__":
     opt = parse_opt()
-    opt.data = 'data/single_toy_iid.yaml'
+    opt.data = 'data/iid-tv.yaml'
     opt.exist_ok = False
     opt.batch_size = 16
-    opt.epochs = 1
+    opt.epochs = 300
     opt.patience = 0
     main(opt)
